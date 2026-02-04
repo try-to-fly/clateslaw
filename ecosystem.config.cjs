3ed5a85e98c8d26f3be39a56a6b1a85aaef4a5be
fox <fox@fox.tail6381c.ts.net>
2026-02-04 18:11:26 +0800

    feat: 修改pm2配置

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,16 @@
 module.exports = {
-  apps: [{
-    name: 'tesla-mqtt',
-    script: 'pnpm',
-    args: 'dev mqtt',
-    cwd: '/Users/fox/Documents/works/tesla-service',
-    autorestart: true,
-    max_restarts: 10,
-    restart_delay: 5000,
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    error_file: './logs/mqtt-error.log',
-    out_file: './logs/mqtt-out.log',
-  }]
+  apps: [
+    {
+      name: "tesla-mqtt",
+      script: "pnpm",
+      args: "dev mqtt",
+      cwd: process.cwd(),
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 5000,
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      error_file: "./logs/mqtt-error.log",
+      out_file: "./logs/mqtt-out.log",
+    },
+  ],
 };
